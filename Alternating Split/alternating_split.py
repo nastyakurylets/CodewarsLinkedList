0c104f28528c5495d3eda9f00f2dcb224ad4302a
nastyakurylets <kurylets.pn@ucu.edu.ua>
2024-04-08 13:05:54 +0300

    Add working code for alternating split.

---
+++ Alternating Split/alternating_split.py
@@ -0,0 +1,30 @@
+class Node(object):
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
+
+class Context(object):
+    def __init__(self, first, second):
+        self.first = first
+        self.second = second
+
+def alternating_split(head):
+    if head is None or head.next is None:
+        raise AttributeError
+    first_head = Node(head.data)
+    second_head = Node(head.next.data)
+    first_current = first_head
+    second_current = second_head
+    pointer = head.next.next
+    count = 2
+    while pointer is not None:
+        new_node = Node(pointer.data)
+        if count % 2 == 0:
+            first_current.next = new_node
+            first_current = first_current.next
+        else:
+            second_current.next = new_node
+            second_current = second_current.next
+        pointer = pointer.next
+        count += 1
+    return Context(first_head, second_head)
